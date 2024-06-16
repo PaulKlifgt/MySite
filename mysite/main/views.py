a17c8e4d584ed60dc3d6c48e2561ff33c3617df6
PaulKlifgt <boevo.arty@yandex.ru>
2024-06-16 18:44:03 +1000

    publish

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -2,13 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
-data={
-    'title': 'Главная',
-    'values': ['some', 'none', 'lorem']
-}
-
 def index(request):
-    return render(request, 'main/index.html', data)
+    return render(request, 'main/index.html')
 
 def about(request):
     return render(request, 'main/about.html')
